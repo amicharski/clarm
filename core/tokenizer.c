1ddc01527cda41596dcc3818a7cddac65266e7ff
amicharski <amicharski@outlook.com>
2022-05-15 17:04:00 -0400

    initially completed tokenizer

--- core/tokenizer.c
+++ core/tokenizer.c
@@ -26,6 +26,32 @@ char* peek_range(int range){
     return (char *) range_arr;
 }
 
+bool is_digit(char ch){
+    return ch >= '0' && ch <= '9';
+}
+
+bool is_alpha(char ch){
+    return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z';
+}
+
+bool is_alphanumeric(char ch){
+    return is_alpha(ch) || is_digit(ch);
+}
+
+bool is_symbolic(char ch){
+    return is_alphanumeric(ch) || ch == '-' || ch == '+' || ch == '*' || ch == '!'
+    || ch == '\'' || ch == '?' || ch == '<' || ch == '>' || ch == '=' || ch == '_';
+}
+
+bool is_stringable(char ch){
+    return ch >= ' ' && ch <= '~' && ch != '\\' && ch != '"';
+}
+
+void insert_token(enum TokenType token, char* name){
+    tokens[token_counter++].token_type = token;
+    if(debug)   print_to_shell(name);
+}
+
 Token* tokenize(char* sourceCode){
     source = sourceCode;
     tokens = malloc(1024);
@@ -34,49 +60,66 @@ Token* tokenize(char* sourceCode){
         if(debug)   printf("\nch: %c\n", ch);
         switch(ch){
             case '(':
-                tokens[token_counter++].token_type = LEFT_PAREN;
-                if(debug) print_to_shell("LEFT PARENTHESIS");
+                insert_token(FUNCTION_START, "FUNCTION START");
                 break;
             case ')':
-                tokens[token_counter++].token_type = RIGHT_PAREN;
-                if(debug) print_to_shell("RIGHT PARENTHESIS");
+                insert_token(FUNCTION_END, "FUNCTION END");
                 break;
-            case '[':
-                tokens[token_counter++].token_type = LEFT_BRACKET;
-                if(debug) print_to_shell("LEFT BRACKET");
+            case '+':
+                insert_token(ADD, "ADD");
                 break;
-            case ']':
-                tokens[token_counter++].token_type = RIGHT_BRACKET;
-                if(debug) print_to_shell("RIGHT BRACKET");
+            case '-':
+                insert_token(SUBTRACT, "SUBTRACT");
                 break;
-            case '{':
-                tokens[token_counter++].token_type = LEFT_BRACE;
-                if(debug) print_to_shell("LEFT BRACE");
+            case '*':
+                insert_token(MULTIPLY, "MULTIPLY");
                 break;
-            case '}':
-                tokens[token_counter++].token_type = RIGHT_BRACE;
-                if(debug) print_to_shell("RIGHT BRACE");
+            case '/':
+                insert_token(DIVIDE, "DIVIDE");
                 break;
-            case '^':
-                tokens[token_counter++].token_type = CIRCUMFLEX;
-                if(debug) print_to_shell("CIRCUMFLEX");
-            case '@':
-                tokens[token_counter++].token_type = COMMERCIAL_AT;
-                if(debug) print_to_shell("COMMERCIAL AT");
-            case '#':
-                tokens[token_counter++].token_type = POUND;
-                if(debug) print_to_shell("POUND");
-            case '`':
-                tokens[token_counter++].token_type = SYNTAX_QUOTE;
-                if(debug) print_to_shell("SYNTAX QUOTE");
-            case '~':
-                if(peek_next() == '@'){
-                    tokens[token_counter++].token_type = UNQUOTE_SPLICING;
-                    if(debug) print_to_shell("UNQUOTE SPLICING");
+            case '\\':
+                insert_token(CHAR, "CHAR");
+                ch += 2;
+            case '>':
+                if(peek_next() == '='){
+                    insert_token(GREATER_THAN_OR_EQUAL_TO,
+                                 "GREATER_THAN_OR_EQUAL_TO");
+                    ch += 2;
                 } else {
-                    tokens[token_counter++].token_type = UNQUOTE;
-                    if(debug) print_to_shell("UNQUOTE");
+                    insert_token(GREATER_THAN, "GREATER_THAN");
+                    ch++;
                 }
+            case '<':
+                if(peek_next() == '='){
+                    insert_token(LESS_THAN_OR_EQUAL_TO,
+                                 "LESS_THAN_OR_EQUAL_TO");
+                    ch += 2;
+                } else {
+                    insert_token(LESS_THAN, "LESS_THAN");
+                    ch++;
+                }
+            case '"':
+                while(is_stringable(ch))    ch++;
+                if(peek_next() == '"'){
+                    ch++;
+                    insert_token(STRING, "STRING");
+                }
+            default:
+                if(is_digit(ch)){
+                    while(is_digit(ch++)){}
+                    if(peek_next() == '.'){
+                        if(is_digit(ch)){
+                            while(is_digit(ch++)){}
+                            insert_token(FLOAT, "FLOAT");
+                        }
+                    } else {
+                        insert_token(INT, "INT");
+                    }
+                } else if(is_symbolic(ch)){
+                    while(is_symbolic(ch++)){}
+                    insert_token(SYMBOL, "SYMBOL");
+                }
+                break;
         }
     } while(sourceCode[i++] != '\0');
     tokens[token_counter++].token_type = END_OF_FILE;
